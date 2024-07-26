a03e7fef6a3b45810df2b39a72d73441c12c64ad
hezou <hezoubeke18@gmail.com>
2024-07-26 10:02:44 +0000

    service update with token

--- Easy_front/src/Components/Home.jsx
+++ Easy_front/src/Components/Home.jsx
@@ -42,8 +42,7 @@ function Home (){
             </div>
         </nav>
     </header>
-    )
+    );
 }
-
 export default Home;
 
--- Easy_front/src/services/authService.js
+++ Easy_front/src/services/authService.js
@@ -2,19 +2,41 @@ import axios from 'axios';
 const API_URL = 'http://localhost:7000/api/auth/';
 
 const signup = (userData) => {
-   return axios.post(API_URL + 'signup', userData);
+    return axios.post(API_URL + 'signup', userData);
 }
-const login = ( email, password) => {
-    return axios.post(API_URL + 'login', {
-        email,
-        password
-    });
-   
-};
+
+const login = (email, password) => {
+    return axios.post(API_URL + 'login', { email, password })
+        .then(response => {
+            if (response.data.token) {
+                localStorage.setItem('user', JSON.stringify(response.data));
+            }
+            return response.data;
+        });
+}
+
+const logout = () => {
+    localStorage.removeItem('user');
+}
+
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem('user'));
+}
+
+const authHeader = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.token) {
+        return { Authorization: 'Bearer ' + user.token };
+    } else {
+        return {};
+    }
+}
 
 export default {
+    signup,
     login,
-    signup,   
+    logout,
+    getCurrentUser,
+    authHeader
 };
-
-
