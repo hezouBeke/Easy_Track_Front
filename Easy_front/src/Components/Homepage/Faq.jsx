53641f10aac087908971bb2d11c4657d07fbe2d2
hezou <hezoubeke18@gmail.com>
2024-08-15 21:14:00 +0000

    faq end

--- Easy_front/src/Components/Homepage/Faq.jsx
+++ Easy_front/src/Components/Homepage/Faq.jsx
@@ -1,79 +1,65 @@
-function Faq () {
-    return(
-        <section class="bg-white dark:bg-gray-200">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-            <h2 class="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">Frequently asked questions</h2>
-            <div class="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
-                <div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            What do you mean by "Figma assets"?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.</p>
+function Features() {
+    return (
+        <section className="bg-white dark:bg-gray-200">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+                <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">Découvrez les fonctionnalités de notre plateforme</h2>
+                <div className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+                    <div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                            Suivi en temps réel de vos colis
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Grâce à notre technologie de pointe, vous pouvez suivre l'emplacement exact de vos colis en temps réel. Restez informé de chaque étape du processus de livraison, du départ à l'arrivée.</p>
+                        </div>
+                        <div className="mb-10">                        
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Notifications personnalisées
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Recevez des notifications instantanées à chaque étape clé de la livraison. Nos alertes vous tiennent informé des mises à jour importantes, vous permettant de garder une longueur d'avance.</p>
+                        </div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Interface utilisateur intuitive
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Notre plateforme est conçue pour être simple d'utilisation, avec une interface claire et intuitive qui facilite la gestion de vos expéditions. Retrouvez toutes les informations essentielles en un seul endroit.</p>
+                        </div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Options de livraison flexibles
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Proposez à vos clients des options de livraison adaptées à leur emploi du temps. Que ce soit pour une livraison express ou programmée, notre plateforme s'adapte à vos besoins.</p>
+                        </div>
                     </div>
-                    <div class="mb-10">                        
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            What does "lifetime access" exactly mean?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">Once you have purchased either the design, code, or both packages, you will have access to all of the future updates based on the roadmap, free of charge.</p>
-                    </div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            How does support work?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">We're aware of the importance of well qualified support, that is why we decided that support will only be provided by the authors that actually worked on this project.</p>
-                        <p class="text-gray-500 dark:text-gray-400">Feel free to <a href="#" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">contact us</a> and we'll help you out as soon as we can.</p>
-                    </div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            I want to build more than one project. Is that allowed?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">You can use Windster for an unlimited amount of projects, whether it's a personal website, a SaaS app, or a website for a client. As long as you don't build a product that will directly compete with Windster either as a UI kit, theme, or template, it's fine.</p>
-                        <p class="text-gray-500 dark:text-gray-400">Find out more information by <a href="#" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">reading the license</a>.</p>
-                    </div>
-                </div>
-                <div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            What does "free updates" include?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">The free updates that will be provided is based on the <a href="#" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">roadmap</a> that we have laid out for this project. It is also possible that we will provide extra updates outside of the roadmap as well.</p>
-                    </div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            What does the free version include?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">The <a href="#" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">free version</a> of Windster includes a minimal style guidelines, component variants, and a dashboard page with the mobile version alongside it.</p>
-                        <p class="text-gray-500 dark:text-gray-400">You can use this version for any purposes, because it is open-source under the MIT license.</p>
-                    </div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            What is the difference between Windster and Tailwind UI?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">Although both Windster and Tailwind UI are built for integration with Tailwind CSS, the main difference is in the design, the pages, the extra components and UI elements that Windster includes.</p>
-                        <p class="text-gray-500 dark:text-gray-400">Additionally, Windster is a project that is still in development, and later it will include both the application, marketing, and e-commerce UI interfaces.</p>
-                    </div>
-                    <div class="mb-10">
-                        <h3 class="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
-                            <svg class="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg>
-                            Can I use Windster in open-source projects?
-                        </h3>
-                        <p class="text-gray-500 dark:text-gray-400">Generally, it is accepted to use Windster in open-source projects, as long as it is not a UI library, a theme, a template, a page-builder that would be considered as an alternative to Windster itself.</p>
-                        <p class="text-gray-500 dark:text-gray-400">With that being said, feel free to use this design kit for your open-source projects.</p>
-                        <p class="text-gray-500 dark:text-gray-400">Find out more information by <a href="#" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">reading the license</a>.</p>
+                    <div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Historique détaillé des expéditions
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Accédez à l'historique complet de vos expéditions, incluant les informations de suivi et les rapports de performance. Gardez une trace de chaque étape pour une gestion optimale.</p>
+                        </div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Sécurité et confidentialité
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Nous prenons la sécurité de vos données au sérieux. Toutes les informations relatives à vos expéditions sont protégées par des protocoles de sécurité avancés, garantissant leur confidentialité.</p>
+                        </div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Support client réactif
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">Notre équipe de support est à votre disposition pour répondre à toutes vos questions et vous aider à chaque étape. Profitez d'une assistance personnalisée et rapide.</p>
+                        </div>
+                        <div className="mb-10">
+                            <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-black">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M360-120q-100 0-170-70t-70-170v-240q0-100 70-170t170-70h240q100 0 170 70t70 170v240q0 100-70 170t-170 70H360Zm80-200 240-240-56-56-184 184-88-88-56 56 144 144Zm-80 120h240q66 0 113-47t47-113v-240q0-66-47-113t-113-47H360q-66 0-113 47t-47 113v240q0 66 47 113t113 47Zm120-280Z"/></svg>                                Intégration fluide avec vos outils
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400">EasyTrack s'intègre facilement avec vos systèmes existants, simplifiant ainsi la gestion de vos expéditions sans perturber vos opérations actuelles.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-      </section>
+        </section>
     );
 }
 
-export default Faq;
+export default Features;
--- Easy_front/src/Components/Homepage/Headsection.jsx
+++ Easy_front/src/Components/Homepage/Headsection.jsx
@@ -17,8 +17,8 @@ function Headsection() {
             {/* Deuxième partie */}
             <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div className="grid grid-cols-2 gap-4 mt-8">
-    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="Livraison à domicile rapide"/>
-    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="Suivi en temps réel de la livraison"/>
+    <img className="w-full rounded-lg" src="src\assets\im3.jpg" alt="Livraison à domicile rapide"/>
+    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="src\assets\im5.jpg" alt="Suivi en temps réel de la livraison"/>
 </div>
 <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">Livraison à Domicile Simplifiée</h2>
