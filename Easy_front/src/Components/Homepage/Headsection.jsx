a3b0d847cf054ec4899759b7cc92dfe63bf5a1cf
hezou <hezoubeke18@gmail.com>
2024-08-15 20:57:55 +0000

    add fac for description

--- Easy_front/src/Components/Homepage/Headsection.jsx
+++ Easy_front/src/Components/Homepage/Headsection.jsx
@@ -17,16 +17,15 @@ function Headsection() {
             {/* Deuxième partie */}
             <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div className="grid grid-cols-2 gap-4 mt-8">
-                    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="Technologie de suivi avancée"/>
-                    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="Informations de livraison en temps réel"/>
-                </div>
-                <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">Sécurisez vos livraisons avec précision</h2>
-                    <p className="mb-4">Notre technologie avancée vous permet de suivre en temps réel chaque étape de la livraison de vos colis. Avec EasyTrack, dites adieu aux incertitudes et rassurez vos clients avec des informations précises et à jour.</p>
-                    <p>Nous vous offrons les outils nécessaires pour anticiper les retards, éviter les pertes et garantir que vos envois arrivent à destination, dans les meilleures conditions.</p>
-                </div>
-                
-            </div>
+    <img className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="Livraison à domicile rapide"/>
+    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="Suivi en temps réel de la livraison"/>
+</div>
+<div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+    <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black">Livraison à Domicile Simplifiée</h2>
+    <p className="mb-4">Avec EasyTrack, suivez vos colis en temps réel et recevez-les directement chez vous, en toute tranquillité. Profitez de notifications précises pour savoir exactement quand votre colis arrivera.</p>
+    <p>Choisissez des options de livraison flexibles adaptées à votre emploi du temps, pour une expérience de livraison à domicile sans stress.</p>
+</div>
+</div>
         </section>
     );
 }
--- Easy_front/src/Components/Homepage/Home.jsx
+++ Easy_front/src/Components/Homepage/Home.jsx
@@ -6,12 +6,14 @@ import Footer from "./Footer";
 import Headsection from "./Headsection";
 import Socialproof from "./Socialproof";
 import Ctagraph from "./Ctagraph";
+import Faq from "./Faq";
  function Home (){
     return(
       <div>
         <Header /> 
         <Firstsection />
         <Secondsection /> 
+        <Faq />  
         <Headsection />
         <Ctagraph /> 
         <Socialproof />
