1aa931b66da5dc92148d47abb4e5cd3f59bbf2bf
hezou <hezoubeke18@gmail.com>
2024-08-16 12:02:26 +0000

    register update

--- Easy_front/src/Components/Login.jsx
+++ Easy_front/src/Components/Login.jsx
@@ -39,6 +39,7 @@ const Login = () => {
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
                                 <input type="email" name="email" id="email"  value={email}  onChange={(e) => setEmail(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  placeholder="name@company.com"  required />
                             </div>
+                            
                             <div className="relative">
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
                                 <input
--- Easy_front/src/Components/Register.jsx
+++ Easy_front/src/Components/Register.jsx
@@ -6,8 +6,7 @@ function Register() {
     const [isCourier, setIsCourier] = useState(false);
     const [formData, setFormData] = useState({
         role: 'Client',
-        name: '',
-        firstName: '',
+        completename: '',
         tel: '',
         sex: '',
         email: '',
@@ -66,63 +65,75 @@ function Register() {
                                 Coursier
                             </button>
                         </div>
+
                         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                            {/* Personal Information Section */}
                             <div>
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
-                                <input type="text" name="name" id="name" onChange={handleChange} value={formData.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Votre nom" required=""/>
-                            </div>
-                            <div>
-                                <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
-                                <input type="text" name="firstName" id="firstName" onChange={handleChange} value={formData.firstName} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-primary-600" placeholder="Votre prénom" required=""/>
-                            </div>
-                            <div>
-                                <label htmlFor="tel" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Téléphone</label>
-                                <input type="tel" name="tel" id="tel" onChange={handleChange} value={formData.tel} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Votre numéro de téléphone" required=""/>
-                            </div>
-                            <div className="flex items-center space-x-4">
-                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sexe</label>
-                                <label className="flex items-center">
-                                    <input type="radio" name="sex" value="M" onChange={handleChange} className="form-radio text-primary-600 focus:ring-primary-600 dark:focus:ring-blue-500" required/>
-                                    <span className="ml-2 text-sm text-gray-900 dark:text-white">Homme</span>
-                                </label>
-                                <label className="flex items-center">
-                                    <input type="radio" name="sex" value="F" onChange={handleChange} className="form-radio text-primary-600 focus:ring-primary-600 dark:focus:ring-blue-500" required/>
-                                    <span className="ml-2 text-sm text-gray-900 dark:text-white">Femme</span>
-                                </label>
-                            </div>
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
-                                <input type="email" name="email" id="email" onChange={handleChange} value={formData.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
-                                <input type="password" name="password" id="password" onChange={handleChange} value={formData.password} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required=""/>
-                            </div>
-                            <div>
-                                <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmer le mot de passe</label>
-                                <input type="password" name="confirmPassword" id="confirmPassword" onChange={handleChange} value={formData.confirmPassword} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required=""/>
-                            </div>
-                            {isCourier && (
-                                <div>
-                                    <label htmlFor="vehiculeType" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type de véhicule</label>
-                                    <select name="vehiculeType" id="vehiculeType" onChange={handleChange} value={formData.vehiculeType} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                                        <option value="">Sélectionnez le type de véhicule</option>
-                                        <option value="tricycle">Tricycle</option>
-                                        <option value="moto">Moto</option>
-                                        <option value="voiture">Voiture</option>
-                                    </select>
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                    <div>
+                                        <label htmlFor="completename" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom complet</label>
+                                        <input type="text" name="completename" id="completename" onChange={handleChange} value={formData.completename} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Votre nom" required=""/>
+                                    </div>
+                                
+                                    <div>
+                                        <label htmlFor="tel" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Téléphone</label>
+                                        <input type="tel" name="tel" id="tel" onChange={handleChange} value={formData.tel} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Votre numéro de téléphone" required=""/>
+                                    </div>
+                                    <div className="flex items-center space-x-4">
+                                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sexe</label>
+                                        <label className="flex items-center">
+                                            <input type="radio" name="sex" value="M" onChange={handleChange} className="form-radio text-primary-600 focus:ring-primary-600 dark:focus:ring-blue-500" required/>
+                                            <span className="ml-2 text-sm text-gray-900 dark:text-white">Homme</span>
+                                        </label>
+                                        <label className="flex items-center">
+                                            <input type="radio" name="sex" value="F" onChange={handleChange} className="form-radio text-primary-600 focus:ring-primary-600 dark:focus:ring-blue-500" required/>
+                                            <span className="ml-2 text-sm text-gray-900 dark:text-white">Femme</span>
+                                        </label>
+                                    </div>
                                 </div>
-                            )}
-                            {isCourier && (
-                                <div>
-                                    <label htmlFor="vehiculeBrand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Marque du véhicule</label>
-                                    <input type="text" name="vehiculeBrand" id="vehiculeBrand" onChange={handleChange} value={formData.vehiculeBrand} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Marque du véhicule" required=""/>
+                            </div>
+
+                            {/* Account Details Section */}
+                            <div>
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                    <div>
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">E-mail</label>
+                                        <input type="email" name="email" id="email" onChange={handleChange} value={formData.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
+                                        <input type="password" name="password" id="password" onChange={handleChange} value={formData.password} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required=""/>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmer le mot de passe</label>
+                                        <input type="password" name="confirmPassword" id="confirmPassword" onChange={handleChange} value={formData.confirmPassword} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="••••••••" required=""/>
+                                    </div>
                                 </div>
-                            )}
+                            </div>
+
+                            {/* Vehicle Information Section (For Couriers Only) */}
                             {isCourier && (
                                 <div>
-                                    <label htmlFor="vehiculePlate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Plaque d'immatriculation</label>
-                                    <input type="text" name="vehiculePlate" id="vehiculePlate" onChange={handleChange} value={formData.vehiculePlate} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Plaque d'immatriculation" required=""/>
+                                    <h2 className="text-lg font-semibold text-gray-900 dark:text-white mb-4">Détails sur véhicule</h2>
+                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                        <div>
+                                            <label htmlFor="vehiculeType" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type de véhicule</label>
+                                            <select name="vehiculeType" id="vehiculeType" onChange={handleChange} value={formData.vehiculeType} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                                                <option value="">Sélectionnez le type de véhicule</option>
+                                                <option value="tricycle">Tricycle</option>
+                                                <option value="moto">Moto</option>
+                                                <option value="voiture">Voiture</option>
+                                            </select>
+                                        </div>
+                                        <div>
+                                            <label htmlFor="vehiculeBrand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Marque du véhicule</label>
+                                            <input type="text" name="vehiculeBrand" id="vehiculeBrand" onChange={handleChange} value={formData.vehiculeBrand} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Marque du véhicule" required=""/>
+                                        </div>
+                                        <div>
+                                            <label htmlFor="vehiculePlate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Plaque d'immatriculation</label>
+                                            <input type="text" name="vehiculePlate" id="vehiculePlate" onChange={handleChange} value={formData.vehiculePlate} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Plaque d'immatriculation" required=""/>
+                                        </div>
+                                    </div>
                                 </div>
                             )}
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Créer un compte</button>
