ee1a78c4491da8fc92d01ea7dff8fb1bc291a3d5
hezou <hezoubeke18@gmail.com>
2024-08-31 20:36:58 +0000

    evryting ok

--- Easy_front/src/Components/Admin/Colis.jsx
+++ Easy_front/src/Components/Admin/Colis.jsx
@@ -19,8 +19,9 @@ function Colis() {
         fetchColis();
     }, []);
 
+    
     return (
-        <section className="relative bg-gray-900 text-gray-300 p-12 sm:p-5 min-h-screen flex flex-col">
+        <section className="relative bg-gray-900 text-gray-300 p-12 sm:p-10 min-h-screen flex flex-col">
             <Adminheader />
             <div className="flex">
                 <Adminsidebar />
@@ -28,8 +29,15 @@ function Colis() {
                     <div className="bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden ml-64 ">
                         
                         {/* Ajout de la barre de recherche et des filtres */}
-                        <div className="flex justify-between items-center bg-gray-100 p-4 border-b-[3px] border-gray-150">
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">More filters</button>
+                        <div className="flex justify-end items-center bg-gray-100 p-4 border-b-[1px] border-gray-300 space-x-2">
+                            <button className="bg-gray-200 p-2 rounded-lg flex items-center space-x-2">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000">
+                                        <path d="M456-144v-240h72v84h288v72H528v84h-72Zm-312-84v-72h240v72H144Zm144-132v-84H144v-72h144v-84h72v240h-72Zm144-84v-72h384v72H432Zm144-132v-240h72v84h168v72H648v84h-72Zm-432-84v-72h384v72H144Z"/>
+                                    </svg>
+                                </span>
+                                <span>More filters</span>
+                            </button>
                             <div className="flex items-center space-x-2">
                                 <input
                                     type="text"
@@ -40,35 +48,35 @@ function Colis() {
                             </div>
                         </div>
         
-                    <div className="overflow-x-auto mt-0">
-                        <table className="w-full text-sm text-right text-gray-900">
-                        <thead className="text-xs text-gray-900 bg-gray-900 dark:bg-gray-100 dark:text-gray-900 border-t-[1px] border-gray-400 font-thin">
-                                        <tr>
-                                            <th scope="col" className="px-4 py-3">Code Colis</th>
-                                            <th scope="col" className="px-4 py-3">Description Colis</th>
-                                            <th scope="col" className="px-4 py-3">Nom & Prénom du Client</th>
-                                            <th scope="col" className="px-4 py-3">Email</th>
-                                            <th scope="col" className="px-4 py-3">Téléphone</th>
-                                        </tr>
-                                    </thead>
+                        <div className="overflow-x-auto mt-0">
+                            <table className="w-full text-sm text-right text-gray-900">
+                                <thead className="text-xs text-gray-900 bg-gray-900 dark:bg-gray-100 dark:text-gray-900 border-t-[1px] border-gray-400 font-thin">
+                                    <tr>
+                                        <th scope="col" className="px-4 py-3">Code Colis</th>
+                                        <th scope="col" className="px-4 py-3">Description Colis</th>
+                                        <th scope="col" className="px-4 py-3">Nom & Prénom du Client</th>
+                                        <th scope="col" className="px-4 py-3">Email</th>
+                                        <th scope="col" className="px-4 py-3">Téléphone</th>
+                                    </tr>
+                                </thead>
 
-                                    <tbody>
-                                        {colis.map(colisItem => (
-                                            <tr key={colisItem._id} className="hover:bg-gray-900 dark:hover:bg-gray-600">
-                                                <td className="px-4 py-3">{colisItem.client_id.completename}</td>
-                                                <td className="px-4 py-3">{colisItem.description}</td>
-                                                <td className="px-4 py-3">{colisItem.client_id.completename}</td>
-                                                <td className="px-4 py-3">{colisItem.client_id.email || 'N/A'}</td>
-                                                <td className="px-4 py-3">{colisItem.client_id.tel || 'N/A'}</td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                        </table>
+                                <tbody>
+                                    {colis.map(colisItem => (
+                                        <tr key={colisItem._id} className="hover:bg-gray-900 dark:hover:bg-gray-600">
+                                            <td className="px-4 py-3">{colisItem.client_id.completename}</td>
+                                            <td className="px-4 py-3">{colisItem.description}</td>
+                                            <td className="px-4 py-3">{colisItem.client_id.completename}</td>
+                                            <td className="px-4 py-3">{colisItem.client_id.email || 'N/A'}</td>
+                                            <td className="px-4 py-3">{colisItem.client_id.tel || 'N/A'}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
     );
 }
 
--- Easy_front/src/Components/Admin/Expeditions.jsx
+++ Easy_front/src/Components/Admin/Expeditions.jsx
@@ -22,69 +22,72 @@ function Expeditions() {
     }, []);
 
     return (
-        <section className="relative bg-gray-900 text-gray-300 p-12 sm:p-5 min-h-screen flex flex-col">
-    <Adminheader />
-    <div className="flex">
-        <Adminsidebar />
-        <div className="flex-1 mx-auto w-full px-4 lg:px-12 mt-12">
-            <div className="bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden ml-64 ">
-                
-                {/* Ajout de la barre de recherche et des filtres */}
-                <div className="flex justify-between items-center bg-gray-100 p-4 border-b border-gray-300">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">More filters</button>
-                    <div className="flex items-center space-x-2">
-                        <input
-                            type="text"
-                            placeholder="Search"
-                            className="px-4 py-2 border rounded-lg text-black"
-                        />
-                        <button className="text-gray-800">⌘ K</button>
+        <section className="relative bg-gray-900 text-gray-300 p-20 sm:p-10 min-h-screen flex flex-col">
+            <Adminheader />
+            <div className="flex">
+                <Adminsidebar />
+                <div className="flex-1 mx-auto w-full max-w-screen-5xl lg:px-10 mt-12">
+                    <div className="bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden ml-64 ">
+                        {/* Ajout de la barre de recherche et des filtres */}
+                        <div className="flex justify-end items-center bg-gray-100 p-4 border-b border-gray-300 space-x-2">
+                            <button className="bg-gray-200 p-2 rounded-lg flex items-center space-x-2">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000">
+                                        <path d="M456-144v-240h72v84h288v72H528v84h-72Zm-312-84v-72h240v72H144Zm144-132v-84H144v-72h144v-84h72v240h-72Zm144-84v-72h384v72H432Zm144-132v-240h72v84h168v72H648v84h-72Zm-432-84v-72h384v72H144Z"/>
+                                    </svg>
+                                </span>
+                                <span>More filters</span>
+                            </button>
+                            <div className="flex items-center space-x-2">
+                                <input
+                                    type="text"
+                                    placeholder="Search"
+                                    className="px-4 py-2 border rounded-lg text-black"
+                                />
+                                <button className="text-gray-800">⌘ K</button>
+                            </div>
+                        </div>
+
+                        {/* Table */}
+                        <div className="overflow-x-auto mt-0">
+                            <table className="w-full text-sm text-right text-gray-900">
+                                <thead className="text-xs text-gray-900 font-thin bg-gray-100 w-full border-t border-gray-300">
+                                    <tr>
+                                        <th scope="col" className="px-6 py-4 w-1/6">Départ</th>
+                                        <th scope="col" className="px-6 py-4 w-1/6">Destination</th>
+                                        <th scope="col" className="px-6 py-4 w-1/6">Date de Départ</th>
+                                        <th scope="col" className="px-6 py-4 w-1/6">Date d'Arrivée</th>
+                                        <th scope="col" className="px-6 py-4 w-1/6">Client</th>
+                                        <th scope="col" className="px-6 py-4 w-1/6">E-mail</th>
+                                        <th scope="col" className="px-6 py-4 w-1/6">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {expeditions.map(expedition => (
+                                        <tr key={expedition._id} className="hover:bg-gray-700">
+                                            <td className="px-6 py-4">{expedition.lieu_depart}</td>
+                                            <td className="px-6 py-4">{expedition.lieu_arrivee}</td>
+                                            <td className="px-6 py-4">{expedition.date_depart}</td>
+                                            <td className="px-6 py-4">{expedition.date_arrivee}</td>
+                                            <td className="px-6 py-4">{expedition.client_id.completename}</td>
+                                            <td className="px-6 py-4">{expedition.client_id.email}</td>
+                                            <td className="px-6 py-4 text-right">
+                                                <button 
+                                                    onClick={() => navigate(`/expeditions/${expedition._id}`)}
+                                                    className="text-blue-400 hover:text-blue-600"
+                                                >
+                                                    Voir Détail
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-
-                {/* Table */}
-                <div className="overflow-x-auto mt-0">
-                    <table className="w-full text-sm text-right text-gray-900">
-                        <thead className="text-xs text-gray-900 font-thin bg-gray-100 w-full border-t border-gray-300">
-                            <tr>
-                                <th scope="col" className="px-6 py-4 w-1/6">Départ</th>
-                                <th scope="col" className="px-6 py-4 w-1/6">Destination</th>
-                                <th scope="col" className="px-6 py-4 w-1/6">Date de Départ</th>
-                                <th scope="col" className="px-6 py-4 w-1/6">Date d'Arrivée</th>
-                                <th scope="col" className="px-6 py-4 w-1/6">Client</th>
-                                <th scope="col" className="px-6 py-4 w-1/6">E-mail</th>
-                                <th scope="col" className="px-6 py-4 w-1/6">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {expeditions.map(expedition => (
-                                <tr key={expedition._id} className="hover:bg-gray-700">
-                                    <td className="px-6 py-4">{expedition.lieu_depart}</td>
-                                    <td className="px-6 py-4">{expedition.lieu_arrivee}</td>
-                                    <td className="px-6 py-4">{expedition.date_depart}</td>
-                                    <td className="px-6 py-4">{expedition.date_arrivee}</td>
-                                    <td className="px-6 py-4">{expedition.client_id.completename}</td>
-                                    <td className="px-6 py-4">{expedition.client_id.email}</td>
-                                    <td className="px-6 py-4 text-right">
-                                        <button 
-                                            onClick={() => navigate(`/expeditions/${expedition._id}`)}
-                                            className="text-blue-400 hover:text-blue-600"
-                                        >
-                                            Voir Détail
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
             </div>
-        </div>
-    </div>
-</section>
-
-
-    
+        </section>
     );
 }
 
