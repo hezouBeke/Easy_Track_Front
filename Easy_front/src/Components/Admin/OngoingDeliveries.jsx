5d7ee4ffd8b3f3544da37ab5fb0cf68fff61092f
hezou <hezoubeke18@gmail.com>
2024-09-18 19:09:05 +0000

    updatr

--- Easy_front/src/Components/Admin/OngoingDeliveries.jsx
+++ Easy_front/src/Components/Admin/OngoingDeliveries.jsx
@@ -13,119 +13,120 @@ const MessageIcon = () => (
 );
 
 const OngoingDeliveries = () => {
-  const deliveries = [
-    {
-      id: "KG3200L3122324GF",
-      departure: "22.08.21 16:40 PM",
-      arrival: "24.08.21 12:30 PM",
-      customer: "Ella Doer",
-      price: "1334 $",
-      description: "Clothes",
-      weight: "1,2 kg",
-      driver: "John Green",
-      status: "In Transit",
-    },
-  ];
-
-  return (
-    <div className="flex flex-col items-start space-y-10 p-8 mt-6 w-full lg:ml-[-2] lg:mr-15"> {/* Ajustement avec ml-[-2] pour déplacer légèrement vers la gauche */}
-      {/* SECTION Ajouter un colis */}
-      <div className="p-6 bg-gradient-to-r from-gray-800 to-black rounded-lg shadow-md w-full">
-        <h2 className="text-xl font-semibold text-white">Ajouter un colis</h2>
-        <p className="text-sm text-gray-400 mt-2">Remplissez le formulaire pour créer un nouveau colis</p>
-
-        <div className="relative mt-4">
-          <input
-            type="text"
-            placeholder="Numéro de suivi"
-            className="w-full px-4 py-3 text-white bg-gray-800 border border-gray-500 rounded-full focus:outline-none focus:border-gray-400 placeholder-gray-400"
-          />
-          <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full hover:bg-gray-300 transition-all">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14M12 5l7 7-7 7" />
-            </svg>
-          </button>
+    const deliveries = [
+      {
+        id: "KG3200L3122324GF",
+        departure: "22.08.21 16:40 PM",
+        arrival: "24.08.21 12:30 PM",
+        customer: "Ella Doer",
+        price: "1334 $",
+        description: "Clothes",
+        weight: "1,2 kg",
+        driver: "John Green",
+        status: "In Transit",
+      },
+    ];
+  
+    return (
+      <div className="flex flex-col items-start space-y-6 p-8 mt-6 w-full lg:ml-[-2] lg:mr-15"> {/* Ajustement du `space-y-10` à `space-y-6` pour réduire l'espace */}
+        {/* SECTION Ajouter un colis */}
+        <div className="p-6 bg-gradient-to-r from-gray-800 to-black rounded-lg shadow-md w-full">
+          <h2 className="text-xl font-semibold text-white">Ajouter un colis</h2>
+          <p className="text-sm text-gray-400 mt-2">Remplissez le formulaire pour créer un nouveau colis</p>
+  
+          <div className="relative mt-4">
+            <input
+              type="text"
+              placeholder="Numéro de suivi"
+              className="w-full px-4 py-3 text-white bg-gray-800 border border-gray-500 rounded-full focus:outline-none focus:border-gray-400 placeholder-gray-400"
+            />
+            <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-white text-black p-2 rounded-full hover:bg-gray-300 transition-all">
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </button>
+          </div>
+        </div>
+  
+        {/* Colis expédiés */}
+        <div className="p-3 bg-white text-black rounded-lg shadow-lg mt-6 w-full"> {/* Ajustement du `mt-10` à `mt-6` */}
+          {/* En-tête figée avec fond bleu */}
+          <div className="flex justify-between items-center p-4 bg-blue-500 text-white rounded-t-none shadow-md w-full">
+            <h2 className="text-xl font-thin">Colis expédiés</h2>
+          </div>
+  
+          {/* Contenu défilant */}
+          <div className="pt-4 space-y-3">
+            {deliveries.map((delivery, index) => (
+              <div
+                key={index}
+                className="p-3 mb-3 bg-white rounded-lg border border-gray-200 shadow-md hover:shadow-blue-400 transition-shadow text-sm font-thin w-full"
+                style={{ transition: 'box-shadow 0.3s ease' }}
+              >
+                <div className="flex justify-between items-center mb-2">
+                  <span className="text-xs font-thin">Numéro de suivi</span>
+                  <span className="px-2 py-1 bg-yellow-400 text-white text-xs rounded-full">{delivery.status}</span>
+                </div>
+                <h3 className="text-md font-semibold text-black">{delivery.id}</h3>
+  
+                <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
+  
+                <div className="flex justify-between items-center mt-2 text-xs">
+                  <div className="text-gray-600">
+                    <p>Départ</p>
+                    <p className="font-semibold">{delivery.departure}</p>
+                  </div>
+                  <div className="text-gray-600">
+                    <p>Arrivée</p>
+                    <p className="font-semibold">{delivery.arrival}</p>
+                  </div>
+                </div>
+  
+                <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
+  
+                <div className="flex justify-between items-center mt-2 text-xs">
+                  <div className="flex-1">
+                    <p className="text-gray-600">Client</p>
+                    <p className="font-semibold">{delivery.customer}</p>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-gray-600">Prix</p>
+                    <p className="font-semibold">{delivery.price}</p>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-gray-600">Description</p>
+                    <p className="font-semibold">{delivery.description}</p>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-gray-600">Poids</p>
+                    <p className="font-semibold">{delivery.weight}</p>
+                  </div>
+                </div>
+  
+                <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
+  
+                <div className="flex items-center mt-4">
+                  <div className="w-8 h-8 rounded-full bg-gray-300 mr-3"></div>
+                  <div className="text-xs">
+                    <p className="text-gray-600">Livreur</p>
+                    <p className="font-semibold">{delivery.driver}</p>
+                  </div>
+                  <div className="ml-auto flex space-x-2">
+                    <button className="bg-black text-white p-2 rounded-full flex justify-center items-center">
+                      <PhoneIcon />
+                    </button>
+                    <button className="bg-black text-white p-2 rounded-full flex justify-center items-center">
+                      <MessageIcon />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-
-      {/* Colis expédiés */}
-      <div className="p-3 bg-white text-black rounded-lg shadow-lg mt-10 w-full">
-        {/* En-tête figée avec fond bleu */}
-        <div className="flex justify-between items-center p-4 bg-blue-500 text-white rounded-t-none shadow-md w-full">
-          <h2 className="text-xl font-thin">Colis expédiés</h2>
-        </div>
-
-        {/* Contenu défilant */}
-        <div className="pt-4 space-y-3">
-          {deliveries.map((delivery, index) => (
-            <div
-              key={index}
-              className="p-3 mb-3 bg-white rounded-lg border border-gray-200 shadow-md hover:shadow-blue-400 transition-shadow text-sm font-thin w-full"
-              style={{ transition: 'box-shadow 0.3s ease' }}
-            >
-              <div className="flex justify-between items-center mb-2">
-                <span className="text-xs font-thin">Numéro de suivi</span>
-                <span className="px-2 py-1 bg-yellow-400 text-white text-xs rounded-full">{delivery.status}</span>
-              </div>
-              <h3 className="text-md font-semibold text-black">{delivery.id}</h3>
-
-              <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
-
-              <div className="flex justify-between items-center mt-2 text-xs">
-                <div className="text-gray-600">
-                  <p>Départ</p>
-                  <p className="font-semibold">{delivery.departure}</p>
-                </div>
-                <div className="text-gray-600">
-                  <p>Arrivée</p>
-                  <p className="font-semibold">{delivery.arrival}</p>
-                </div>
-              </div>
-
-              <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
-
-              <div className="flex justify-between items-center mt-2 text-xs">
-                <div className="flex-1">
-                  <p className="text-gray-600">Client</p>
-                  <p className="font-semibold">{delivery.customer}</p>
-                </div>
-                <div className="flex-1">
-                  <p className="text-gray-600">Prix</p>
-                  <p className="font-semibold">{delivery.price}</p>
-                </div>
-                <div className="flex-1">
-                  <p className="text-gray-600">Description</p>
-                  <p className="font-semibold">{delivery.description}</p>
-                </div>
-                <div className="flex-1">
-                  <p className="text-gray-600">Poids</p>
-                  <p className="font-semibold">{delivery.weight}</p>
-                </div>
-              </div>
-
-              <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
-
-              <div className="flex items-center mt-4">
-                <div className="w-8 h-8 rounded-full bg-gray-300 mr-3"></div>
-                <div className="text-xs">
-                  <p className="text-gray-600">Livreur</p>
-                  <p className="font-semibold">{delivery.driver}</p>
-                </div>
-                <div className="ml-auto flex space-x-2">
-                  <button className="bg-black text-white p-2 rounded-full flex justify-center items-center">
-                    <PhoneIcon />
-                  </button>
-                  <button className="bg-black text-white p-2 rounded-full flex justify-center items-center">
-                    <MessageIcon />
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default OngoingDeliveries;
+    );
+  };
+  
+  export default OngoingDeliveries;
+  
