45c260f323f939c5736f4a2bc67323b151d4ef04
hezou <hezoubeke18@gmail.com>
2024-10-09 11:03:02 +0000

    update

--- Easy_front/src/Components/MyGoogleMap.jsx
+++ Easy_front/src/Components/MyGoogleMap.jsx
@@ -13,17 +13,17 @@ const center = {
 
 function MyGoogleMap() {
   return (
-    <div className="bg-white shadow-lg transition-shadow duration-300 hover:shadow-blue-500 max-w-4xl h-[400px] lg:h-[542px] lg:ml-[-60px] mt-14 lg:mr-[-20px] rounded-lg"> {/* Ajout de la classe rounded-lg pour arrondir les bords */}
-      <LoadScript googleMapsApiKey="AIzaSyA1k2KoK3gmKQ01mI17Rp8rvaoExxdqsDY">
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={5}
-        >
-          {/* Vous pouvez ajouter des marqueurs ou autres composants ici */}
-        </GoogleMap>
-      </LoadScript>
-    </div>
+    <div className="bg-white shadow-lg transition-shadow duration-300 hover:shadow-blue-500 max-w-4xl h-[400px] lg:h-[542px] lg:ml-[-60px] mt-14 lg:mr-[-20px] rounded-lg">
+    <LoadScript googleMapsApiKey="AIzaSyA1k2KoK3gmKQ01mI17Rp8rvaoExxdqsDY">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={5}
+      >
+        {/* Vous pouvez ajouter des marqueurs ou autres composants ici */}
+      </GoogleMap>
+    </LoadScript>
+  </div>
   );
 }
 
