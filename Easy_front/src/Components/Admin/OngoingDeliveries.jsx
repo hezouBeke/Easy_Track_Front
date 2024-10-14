008e17288bac0f6b0c751a4c42dc9d44b413ea15
hezou <hezoubeke18@gmail.com>
2024-10-14 12:13:48 +0000

    update

--- Easy_front/src/Components/Admin/OngoingDeliveries.jsx
+++ Easy_front/src/Components/Admin/OngoingDeliveries.jsx
@@ -22,14 +22,18 @@ const OngoingDeliveries = () => {
   }
   return (
     <div className="flex flex-col items-start space-y-6 p-8 mt-6 w-full lg:ml-[-2] lg:mr-15">
-      <div className="p-3 bg-white text-black rounded-lg shadow-lg mt-1 w-full">
-        {/* En-tête figée avec fond bleu */}
-        <div className="flex justify-between items-center p-4 bg-blue-500 text-white rounded-t-none shadow-md w-full">
-          <h2 className="text-xl font-thin">Colis expédiés</h2>
-        </div>
-        {/* Contenu avec scroll caché */}
-        <div className="pt-4 space-y-3" style={{ maxHeight: '550px', overflowY: 'auto', scrollbarWidth: 'none' }}>
-          {expeditions.map((expedition, index) => (
+    <div className="p-3 bg-white text-black rounded-lg shadow-lg mt-1 w-full">
+      {/* En-tête figée avec fond bleu */}
+      <div className="flex justify-between items-center p-4 bg-blue-500 text-white rounded-t-none shadow-md w-full">
+        <h2 className="text-xl font-thin">Colis expédiés</h2>
+      </div>
+      {/* Contenu avec scroll caché */}
+      <div className="pt-4 space-y-3" style={{ maxHeight: '550px', overflowY: 'auto', scrollbarWidth: 'none' }}>
+        {expeditions.map((expedition, index) => {
+          const firstCourse = expedition.course_ids?.[0]; // Récupère le premier coursier
+          const coursierActuel = firstCourse?.coursier_id?.completename || 'N/A';
+  
+          return (
             <div
               key={index}
               className="p-3 mb-5 bg-white rounded-lg border border-gray-200 shadow-md hover:shadow-blue-400 transition-shadow text-sm font-thin w-full"
@@ -37,18 +41,18 @@ const OngoingDeliveries = () => {
             >
               <div className="flex justify-between items-center mb-2">
                 <span className="text-xs font-thin">Numéro de suivi</span>
-                <span className="px-2 py-1 bg-yellow-400 text-white text-xs rounded-full">{expedition.status || 'In Transit'}</span>
+                <span className="px-2 py-1 bg-yellow-400 text-white text-xs rounded-full">{expedition?.status || 'In Transit'}</span>
               </div>
-              <h3 className="text-md font-semibold text-black">{expedition.expedition_code}</h3>
+              <h3 className="text-md font-semibold text-black">{expedition?.expedition_code || 'N/A'}</h3>
               <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
               <div className="flex justify-between items-center mt-2 text-xs">
                 <div className="text-gray-600">
                   <p>Départ</p>
-                  <p className="font-semibold">{new Date(expedition.date_depart).toLocaleString()}</p>
+                  <p className="font-semibold">{new Date(expedition?.date_depart).toLocaleString() || 'N/A'}</p>
                 </div>
                 <div className="text-gray-600">
                   <p>Arrivée</p>
-                  <p className="font-semibold">{new Date(expedition.date_arrivee).toLocaleString()}</p>
+                  <p className="font-semibold">{new Date(expedition?.date_arrivee).toLocaleString() || 'N/A'}</p>
                 </div>
               </div>
               <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
@@ -56,19 +60,19 @@ const OngoingDeliveries = () => {
                 {/* Vérifier si "colis" existe avant d'y accéder */}
                 <div className="flex-1">
                   <p className="text-gray-600">Client</p>
-                  <p className="font-semibold">{expedition.colis?.client_id_exp || 'N/A'}</p>
+                  <p className="font-semibold">{expedition?.colis_id?.client_id_exp?.completename || 'N/A'}</p>
                 </div>
                 <div className="flex-1">
                   <p className="text-gray-600">NumColis</p>
-                  <p className="font-semibold">{expedition.colis?.numero_colis || 'N/A'}</p>
+                  <p className="font-semibold">{expedition?.colis_id?.numero_colis || 'N/A'}</p>
                 </div>
                 <div className="flex-1">
                   <p className="text-gray-600">Description</p>
-                  <p className="font-semibold">{expedition.colis?.description || 'N/A'}</p>
+                  <p className="font-semibold">{expedition?.colis_id?.description || 'N/A'}</p>
                 </div>
                 <div className="flex-1">
                   <p className="text-gray-600">Poids</p>
-                  <p className="font-semibold">{expedition.colis?.poids ? `${expedition.colis.poids} kg` : 'N/A'}</p>
+                  <p className="font-semibold">{expedition?.colis_id?.poids ? `${expedition.colis_id.poids} kg` : 'N/A'}</p>
                 </div>
               </div>
               <hr className="my-2 border-gray-300" style={{ borderWidth: '0.5px' }} />
@@ -76,30 +80,32 @@ const OngoingDeliveries = () => {
                 <div className="w-8 h-8 rounded-full bg-gray-300 mr-3"></div>
                 <div className="text-xs">
                   <p className="text-gray-600">Coursier</p>
-                  {/* Vérifier si "course" existe avant d'y accéder */}
-                  <p className="font-semibold">{expedition.course?.coursier_id || 'N/A'}</p>
+                  {/* Vérifier si le coursier existe */}
+                  <p className="font-semibold">{coursierActuel}</p>
                 </div>
                 <div className="ml-40"></div>
                 <div className="flex space-x-5 mt-5 mb-0">
-              <button className="flex items-center justify-center space-x-2 bg-white border border-gray-300 text-black py-1 px-3 rounded-lg hover:bg-gray-100 transition text-xs">
-                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000">
-                  <path d="M760-480q0-117-81.5-198.5T480-760v-80q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480h-80Zm-160 0q0-50-35-85t-85-35v-80q83 0 141.5 58.5T680-480h-80Zm198 360q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/>
-                </svg>
-                <span>Call</span>
-              </button>
-              <button className="flex items-center justify-center space-x-2 bg-blue-500 text-white py-1 px-4 rounded-lg hover:bg-blue-600 transition text-xs">
-                <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
-                  <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
-                </svg>
-                <span>Message</span>
-              </button>
-            </div>
+                  <button className="flex items-center justify-center space-x-2 bg-white border border-gray-300 text-black py-1 px-3 rounded-lg hover:bg-gray-100 transition text-xs">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000">
+                      <path d="M760-480q0-117-81.5-198.5T480-760v-80q75 0 140.5 28.5t114 77q48.5 48.5 77 114T840-480h-80Zm-160 0q0-50-35-85t-85-35v-80q83 0 141.5 58.5T680-480h-80Zm198 360q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12ZM241-600l66-66-17-94h-89q5 41 14 81t26 79Zm358 358q39 17 79.5 27t81.5 13v-88l-94-19-67 67ZM241-600Zm358 358Z"/>
+                    </svg>
+                    <span>Call</span>
+                  </button>
+                  <button className="flex items-center justify-center space-x-2 bg-blue-500 text-white py-1 px-4 rounded-lg hover:bg-blue-600 transition text-xs">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#FFFFFF">
+                      <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
+                    </svg>
+                    <span>Message</span>
+                  </button>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
+          );
+        })}
       </div>
     </div>
+  </div>
+  
   );
 };
 
